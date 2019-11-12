a2838cc8c3e889afd7e4195161449e7789d64f33
atheodosiou <anastasios.theodosiou@gmail.com>
2019-11-13 01:26:02 +0200

    Add cloud code examples

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,17 @@
-Parse.Cloud.define('hello', function(req, res) {
-  return 'Hi';
+//Request to create thumbnail
+//POST => localhost:4242/parse/functions/createthumbnail
+//Body => {
+//  "landmarkId":"0PCIQ6YkH5"  
+//}
+Parse.Cloud.define('createthumbnail', function (req, res) {
+  var landmark = Parse.Object.extend("Landmarks");
+  var query = new Parse.Query(landmark);
+  query.get(req.params.landmarkId)
+    .then((l) => {
+      // const photo = l.get('photo');
+      // console.log(photo)
+      res.success(l);
+    }, (error) => {
+      res.success(error);
+    });
 });
