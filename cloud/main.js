81f1a19fd314f850c57bf88f03e45cf46e5ead43
atheodosiou <anastasios.theodosiou@gmail.com>
2019-11-14 21:30:54 +0200

    create thumbnail functionality

--- cloud/main.js
+++ cloud/main.js
@@ -1,17 +1,31 @@
-//Request to create thumbnail
-//POST => localhost:4242/parse/functions/createthumbnail
-//Body => {
-//  "landmarkId":"0PCIQ6YkH5"  
-//}
-Parse.Cloud.define('createthumbnail', function (req, res) {
-  var landmark = Parse.Object.extend("Landmarks");
-  var query = new Parse.Query(landmark);
-  query.get(req.params.landmarkId)
-    .then((l) => {
-      // const photo = l.get('photo');
-      // console.log(photo)
-      res.success(l);
-    }, (error) => {
-      res.success(error);
-    });
+const sharp = require('sharp');
+
+Parse.Cloud.define('thumbnail', function (req, res) {
+  Parse.Cloud.httpRequest({ url: req.params.url }).then(function (response) {
+    sharp(response.buffer).resize(250, 250).toBuffer()
+      .then(data => {
+        const file = Array.from(Buffer.from(data, "binary"));
+        const parseFile = new Parse.File("resized", file, response.headers['content-type']);
+        parseFile.save().then(saved => {
+          const landmark = Parse.Object.extend("Landmarks");
+          const query = new Parse.Query(landmark);
+
+          query.get(req.params.landmarkId, { useMasterKey: true })
+            .then(querySuccess => {
+              querySuccess.set("photo_thumb", saved);
+              querySuccess.save()
+                .then(saveSuccess => {
+                  res.success({ message: "Landmark updated", landmark: saveSuccess })
+                })
+                .catch(saveFailed => {
+                  res.error({ message: "Landmark failed to be updated", error: saveFailed })
+                })
+            })
+            .catch(queryFailed => {
+              res.error({ message: "Query failed", error: queryFailed })
+            })
+        })
+          .catch(saveFailed => res.error({ saveFailed: saveFailed }));
+      }).catch(sharpError => res.error({ sharpError: sharpError }));
+  }).catch(httpError => res.error({ httpError: httpError }))
 });
