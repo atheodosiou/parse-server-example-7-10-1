9ea708a415f4f1f762c203df4a42e89985aa0bf8
atheodosiou <anastasios.theodosiou@gmail.com>
2019-11-16 16:39:23 +0200

    Updates

--- index.js
+++ index.js
@@ -16,15 +16,10 @@ var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'instashopInterviewAppId',
-  masterKey: process.env.MASTER_KEY || 'instashopInterviewMasterKey', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-  liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+  masterKey: process.env.MASTER_KEY || 'instashopInterviewMasterKey',
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse'
 });
-// Client-keys like the javascript key or the .NET key are not necessary with parse-server
-// If you wish you require them, you can set them as options in the initialization above:
-// javascriptKey, restAPIKey, dotNetKey, clientKey
+
 var app = express(express.json());
 
 // Serve static assets from the /public folder
@@ -35,20 +30,20 @@ var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
-app.get('/', function(req, res) {
-  res.status(200).send('Pasre servr exaple => Instashop screening project.\nApp is running on port: '+ process.env.PORT);
+app.get('/', function (req, res) {
+  res.status(200).send('Pasre servr exaple => Instashop screening project.\nApp is running on port: ' + process.env.PORT);
 });
 
 // There will be a test page available on the /test path of your server url
 // Remove this before launching your app
-app.get('/test', function(req, res) {
+app.get('/test', function (req, res) {
   res.sendFile(path.join(__dirname, '/public/test.html'));
 });
 
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
-httpServer.listen(port, function() {
-    console.log('parse-server-example running on port ' + port + '.');
+httpServer.listen(port, function () {
+  console.log('parse-server-example running on ' + process.env.SERVER_URL + '...');
 });
 
 // This will enable the Live Query real-time server
